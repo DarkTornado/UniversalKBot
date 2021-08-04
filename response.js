47be0bb9961042c623ccbbfad23a849737cb1d43
Dark Tornado <dt3141592@naver.com>
2021-08-04 09:40:34 +0900

    시간 정보 추가

--- response.js
+++ response.js
@@ -1,3 +1,7 @@
+/*
+만능봇
+© 2021 Dark Tornado, All rights reserved.
+*/
 const ZoneID = {
     "서울": 1159068000,
     "부산": 2611053000,
@@ -20,35 +24,48 @@ const ZoneID = {
     "제주": 5011059000
 };
 
+const Tools = {};
+Tools.getWeather = (zone) => {
+    if (!ZoneID.hasOwnProperty(zone)) return "해당 위치를 찾을 수 없어요";
+    var data = org.jsoup.Jsoup.connect("http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=" + ZoneID[zone])
+        .get();
+    var time = data.select("pubDate").text();
+    time = time.substring(time.indexOf(" ") + 1);
+    var location = data.select("category").text();
+    data = data.select("data").get(0);
+    var status = data.select("wfKor").text();
+    var tmp = data.select("temp").text();
+    var hum = data.select("reh").text();
+
+    var result = "현재날씨 \"" + status + "\"\n\n";
+    result += "기온 - " + tmp + "도\n";
+    result += "습도 - " + hum + "%\n\n";
+    result += "기상청 측정시간 :\n";
+    result += time + "\n\n";
+    result += "기상청 측정위치 :\n";
+    result += location;
+    return result;
+};
+
+
 function response(room, msg, sender, isGroupChat, replier) {
+    
+    /* 날씨 */
     if (msg == "/만능 날씨") {
         replier.reply("날씨는 이렇게 물어보세요.\n\n" +
             "\"예시) 만능 날씨 서울\"\n\n" +
             "위치 리스트\n" +
             "서울,부산,춘천,강릉,인천,수원,청주,홍성,대전,안동,포항,울산,대구,전주,목포,광주,여수,창원,제주");
     } else if (msg.startsWith("/만능 날씨 ")) {
-        var name = msg.replace("만능 날씨 ", "");
-        if (!ZoneID.hasOwnProperty(name)) {
-            replier.reply("해당 위치를 찾을 수 없어요");
-            return;
-        }
-        var data = org.jsoup.Jsoup.connect("http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=" + ZoneID[name])
-            .get();
-        var time = data.select("pubDate").text();
-        time = time.substring(time.indexOf(" ") + 1);
-        var location = data.select("category").text();
-        data = data.select("data").get(0);
-        var status = data.select("wfKor").text();
-        var tmp = data.select("temp").text();
-        var hum = data.select("reh").text();
-
-        var result = "현재날씨 \"" + status + "\"\n\n";
-        result += "기온 - " + tmp + "도\n";
-        result += "습도 - " + hum + "%\n\n";
-        result += "기상청 측정시간 :\n";
-        result += time + "\n\n";
-        result += "기상청 측정위치 :\n";
-        result += location;
-        replier.reply(result);
+        var zone = msg.replace("/만능 날씨 ", "");
+        replier.reply(Tools.getWeather(zone));
     }
+
+    /* 시간 */
+    if (msg == "/만능 몇시야") {
+        var day = new Date();
+        replier.reply("지금 시각은 " + (day.getMonth() + 1) + "월 " + day.getDate() + "일 " +
+            day.getHours() + "시 " + day.getMinutes() + "분 입니다.");
+    }
+
 }
